523c0675aeffd16c83baa21891b6d2fa60db63c2
Okkan Chávez <ochavezg2@miumg.edu.gt>
2022-05-14 10:40:57 -0600

    ejercicio sobre vectores

---
+++ Vectores.java
@@ -0,0 +1,17 @@
+package vectores;
+
+public class Vectores {
+
+    public static void main(String[] args) {
+        
+         int numeroscien [] = new int [100];
+         for(int i=0; i<numeroscien.length; i++){
+           
+           numeroscien[i]=i+1;
+           System.out.println(numeroscien[i]);   
+ 
+         }
+        
+    }
+    
+}
